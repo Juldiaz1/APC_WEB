b7d92f16fe9f9c4f32dedcc0f17c2ea69d4861be
Julissa D <138619747+Juldiaz1@users.noreply.github.com>
2025-11-20 09:09:37 -0600

    database configuration to config.py
    Added database configuration settings for APC Database.

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-
+# Database configuration for APC Database
+DB_CONFIG = {  
+    'host': '127.0.0.1',
+    'database': 'apc_database',
+    'user': 'root',  # if different change 
+    'password': '',  # available to change depending on personal diffrence 
+    'port': 3306
+}

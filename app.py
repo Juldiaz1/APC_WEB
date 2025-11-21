5bdddeb010f50952e013133b40902ac0f0fd56b9
Julissa D <138619747+Juldiaz1@users.noreply.github.com>
2025-11-20 18:09:45 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app.secret_key = 'apc_secret_key_2025'
 
 def get_db():
     return mysql.connector.connect(
-        host="localhost",
+        host="127.0.0.1",
         user="root",
         password="",
         database="apc_database"
